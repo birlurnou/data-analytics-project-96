a838944ea61a07a3aeee0e30c57a846253d7e69c
birlurnou <111186103+birlurnou@users.noreply.github.com>
2026-05-22 11:24:56 +0500

    update

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,31 +1,33 @@
 -- находим платные клики
-WITH paid_clicks AS (
-    SELECT 
+with paid_clicks as (
+    select
         visitor_id,
         visit_date,
-        source AS utm_source,
-        medium AS utm_medium,
-        campaign AS utm_campaign,
-        ROW_NUMBER() OVER (
-            PARTITION BY visitor_id 
-            ORDER BY visit_date DESC
-        ) AS rn
-    FROM sessions
-    WHERE medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+        source as utm_source,
+        medium as utm_medium,
+        campaign as utm_campaign,
+        row_number() over (
+            partition by visitor_id
+            order by visit_date desc
+        ) as rn
+    from sessions
+    where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 ),
+
 -- находим последний платный клик
-last_paid_click AS (
-    SELECT 
+last_paid_click as (
+    select
         visitor_id,
         visit_date,
         utm_source,
         utm_medium,
         utm_campaign
-    FROM paid_clicks
-    WHERE rn = 1
+    from paid_clicks
+    where rn = 1
 )
+
 -- формируем витрину
-SELECT 
+select
     lpc.visitor_id,
     lpc.visit_date,
     lpc.utm_source,
@@ -36,13 +38,15 @@ SELECT
     l.amount,
     l.closing_reason,
     l.status_id
-FROM last_paid_click lpc
-LEFT JOIN leads l ON lpc.visitor_id = l.visitor_id
--- проверка, что лид создан во время или после визита, иначе null
-AND (l.created_at >= lpc.visit_date OR l.created_at IS NULL)
-ORDER BY 
-    l.amount DESC NULLS LAST,
-    lpc.visit_date ASC,
-    lpc.utm_source ASC,
-    lpc.utm_medium ASC,
-    lpc.utm_campaign ASC;
+from last_paid_click as lpc
+left join leads as l
+    on lpc.visitor_id = l.visitor_id
+    -- проверка, что лид создан во время или после визита, иначе null
+    and (lpc.visit_date <= l.created_at or l.created_at is null)
+order by
+    l.amount desc nulls last,
+    lpc.visit_date asc,
+    lpc.utm_source asc,
+    lpc.utm_medium asc,
+    lpc.utm_campaign asc
+limit 10;
